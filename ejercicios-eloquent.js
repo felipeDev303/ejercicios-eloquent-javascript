6922831541fe32d76a8036b538fc43f46bbfb1a1
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-06-07 20:39:47 -0400

    Tablero de ajedrez

--- ejercicios-eloquent.js
+++ ejercicios-eloquent.js
@@ -48,3 +48,38 @@ while (numero <= 100) {
   numero++; // contador
 }
 console.log("fin"); // Salida fuera del bucle while
+
+/* 3. Tablero de ajedrez
+
+Escribe un programa que cree un *string* que represente una cuadrícula de 8 × 8, 
+usando caracteres de nueva línea para separar las líneas. En cada posición de la 
+cuadrícula hay un espacio o un carácter "#". Los caracteres deberían de formar un 
+tablero de ajedrez. Pasar este *string* a ``console.log`` 
+debería mostrar algo como esto: 
+  # # # # 
+ # # # # 
+  # # # # 
+ # # # # 
+  # # # # 
+ # # # # 
+  # # # # 
+ # # # #
+
+ Cuando tengas un programa que genere este patrón, define una vinculación 
+ tamaño = 8 y cambia el programa para que funcione con cualquier tamaño, dando 
+ como salida una cuadrícula con el alto y ancho dados
+*/
+
+let size = 8;
+let board = "";
+for (i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    if ((i + j) % 2 == 0) {
+      board += "#";
+    } else {
+      board += " ";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
