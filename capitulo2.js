7a36fb1d9bb0d054132ad818914c2146a0252012
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-09 22:08:57 -0300

    add for

--- capitulo2.js
+++ capitulo2.js
@@ -38,8 +38,66 @@ console.log(Math.max(2, 4)); //->4
 
 console.log(Math.max(2, 4) * 2); //->8
 
-/*Ejercicios
+// Flujo de control líneal
+let elNumero = Number(prompt("Elige un número"));
+console.log("Tu número es la raíz cuadrada de " + elNumero * elNumero);
 
+// Flujo de control condicional (if)
+let elNumero2 = Number(prompt("Elige un número")); // Pedimos un numero (prompt recibe string y se transforma a número con Number)
+if (!Number.isNaN(elNumero2)) {
+  console.log("Tu número es la raíz cuadrada de " + elNumero2 * elNumero2);
+} // Si es número haz esto
+
+// if-else
+let elNumero3 = Number(prompt("Elige un número"));
+if (!Number.isNaN(elNumero3)) {
+  console.log("Tu número es la raíz cuadrada de " + elNumero3 * elNumero3);
+} else {
+  console.log("Te pedí un número :)");
+}
+
+// Ejemplo de bloque sin {} en una sola línea
+if (1 + 1 == 2) console.log("Es verdad");
+// → Es verdad
+
+// else-if
+let numerito = Number(prompt("Elige un número"));
+if (numerito < 10) {
+  console.log("Número pequeño");
+} else if (numerito < 100) {
+  console.log("Número mediano");
+} else {
+  console.log("Número grande");
+}
+
+// while
+
+let numeroWhile = 0;
+while (numeroWhile <= 12) {
+  console.log(numeroWhile);
+  numeroWhile = numeroWhile + 2;
+}
+
+let contador = 0;
+let resultado = 1;
+while (contador < 10) {
+  resultado = resultado * 2;
+  contador = contador + 1;
+}
+console.log(resultado);
+
+let tuNombre;
+do {
+  tuNombre = prompt("Dime tu nombre");
+} while (!tuNombre);
+console.log(tuNombre);
+
+// CICLO FOR
+for (let numeroFor = 0; numeroFor <= 12; numeroFor = numeroFor + 2) {
+  console.log(numeroFor);
+}
+
+/*Ejercicios
 1. Ciclo de un triángulo 
     Escriba un ciclo que haga siete llamadas a ``console.log`` para generar el siguiente triángulo: 
     ``#`` 
