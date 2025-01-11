1e7791e081d6b0b27b2aa84ed4a83363a5593682
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-11 02:37:24 -0300

    add scope js

--- capitulo3.js
+++ capitulo3.js
@@ -22,3 +22,20 @@ const potencia = function (base, exponente) {
   return resultado;
 };
 console.log(potencia(2, 2));
+
+  // SCOPE EN JS
+  let x = 10; // Alcance global
+  if (true) {
+    let y = 20; // alcance local
+    var z = 30; // alcance global
+    console.log(x + y + z); //-> 60
+  }
+  console.log(x + z); //-> 40
+
+// Excepción 
+const dividirEnDos = function(numero){
+  return numero/2;
+};
+let numero =10; // fuera del alcance de la función
+console.log(dividirEnDos(100));
+console.log(numero);
