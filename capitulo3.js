4606b10a5d10a13cb67d2cc7e8325342d4395552
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-12 23:56:50 -0300

    add cierre

--- capitulo3.js
+++ capitulo3.js
@@ -48,3 +48,37 @@ const potencia2 = (base, exponente) => {
   }
   return resultado;
 };
+
+// Argumentos adicionales
+function cuadrado(x) {
+  return x * x;
+}
+console.log(cuadrado(4, true, "erizo"));
+// → 16
+
+function menos(a, b) {
+  if (b === undefined) return -a;
+  else return a - b;
+}
+console.log(menos(10)); //-> -10
+console.log(menos(10, 5)); // -> 5
+
+function potencia3(base, exponente = 2) {
+  let resultado = 1;
+  for (let contador = 0; contador < exponente; contador++) {
+    resultado *= base;
+  }
+  return resultado;
+}
+console.log(potencia(4));
+// → 16
+console.log(potencia(2, 6));
+// → 64
+
+// Cierre
+function multiplicador(factor) {
+  return (numero) => numero * factor;
+}
+let duplicar = multiplicador(2);
+console.log(duplicar(5));
+// → 10
