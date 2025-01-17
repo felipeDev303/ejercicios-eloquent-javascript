b7ba60836b1d4f591b295ba60eb4e9bfe509d649
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-17 02:57:24 -0300

    add recursion

--- Capitulo 3/recursion.js
+++ Capitulo 3/recursion.js
@@ -7,3 +7,23 @@ function potencia(base, exponente) {
   }
 }
 console.log(potencia(2, 3)); // -> 8
+
+// Acertijo: llegar a un objetivo desde 1 mediante 2 caminos (sumando +5 o multiplicando x3)
+function encontrarSolucion(objetivo) {
+  function encontrar(actual, historia) {
+    if (actual == objetivo) {
+      return historia;
+    } else if (actual > objetivo) {
+      return null;
+    } else {
+      return (
+        encontrar(actual + 5, `(${historia}*5)`) ||
+        encontrar(actual * 3, `(${historia}*3)`)
+      );
+    }
+  }
+  return encontrar(1, "1");
+}
+
+console.log(encontrarSolucion(24));
+// -> (((1*3)+5)+3)
