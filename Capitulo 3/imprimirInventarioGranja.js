66b808a66971c4eb29f37367a91b2242dc47acd1
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-17 20:52:40 -0300

    add funciones crecientes

---
+++ Capitulo 3/imprimirInventarioGranja.js
@@ -0,0 +1,14 @@
+function imprimirInventarioGranja(vacas, pollos) {
+  let stringVaca = String(vacas);
+  while (stringVaca.length < 3) {
+    stringVaca = "0" + stringVaca;
+  }
+  console.log(`${stringVaca} Vacas`);
+
+  let stringPollos = String(pollos);
+  while (stringPollos.length < 3) {
+    stringPollos = "0" + stringPollos;
+  }
+  console.log(`${stringPollos} Pollos`);
+}
+imprimirInventarioGranja(7, 11);
