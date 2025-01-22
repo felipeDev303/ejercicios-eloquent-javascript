789c28947c4a11eeddc365eea170176b8e5596a7
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-21 23:52:09 -0300

    modify minimo

--- Capitulo 3/minimo.js
+++ Capitulo 3/minimo.js
@@ -1,1 +1,10 @@
-function min() {}
+function min(x, y) {
+  return Math.min(x, y);
+}
+console.log(min(2, 1));
+
+/* Valores de retorno
+console.log(Math.min(2, 4)); //->2
+console.log(Math.max(2, 4)); //->4
+
+console.log(Math.max(2, 4) * 2); //->8*/
