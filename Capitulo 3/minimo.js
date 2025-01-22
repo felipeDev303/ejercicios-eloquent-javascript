ae07e8bd9132954600555139b50b30c9c0a6f3c5
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-21 23:57:00 -0300

    Corrige la función mínima en minimo.js

--- Capitulo 3/minimo.js
+++ Capitulo 3/minimo.js
@@ -1,10 +1,17 @@
+// Función min con valor de retorno
 function min(x, y) {
   return Math.min(x, y);
 }
 console.log(min(2, 1));
 
-/* Valores de retorno
-console.log(Math.min(2, 4)); //->2
-console.log(Math.max(2, 4)); //->4
+// Función min() como valor
+let min = function (x, y) {
+  return Math.min(x, y);
+};
+console.log(min(3, 8));
 
-console.log(Math.max(2, 4) * 2); //->8*/
+// Función min versión flecha
+let min = (x, y) => {
+  return Math.min(x, y);
+};
+console.log(min(2, 1));
