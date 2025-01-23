6685ce7c4702b4ae21e5d2529a5b49886116645a
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-23 01:02:35 -0300

    modify esPar

--- Capitulo 3/esPar.js
+++ Capitulo 3/esPar.js
@@ -4,6 +4,8 @@ function esPar(n) {
       return true;
     } else if (n === 1) {
       return false;
+    } else if (n < 0) {
+      return console.log("Solo se permiten enteros positivos");
     } else {
       return recursiva(n - 2);
     }
