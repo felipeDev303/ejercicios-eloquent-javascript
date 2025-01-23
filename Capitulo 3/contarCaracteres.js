f4fbb0d13d76ed50bb6abee11e7efd2de8a2185d
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-23 02:45:26 -0300

    add contarCaracteres

--- Capitulo 3/contarCaracteres.js
+++ Capitulo 3/contarCaracteres.js
@@ -6,16 +6,17 @@ N]. El valor devuelto será un string que contiene solo un carácter (por ejempl
 encuentre en la posición string.length - 1. En otras palabras, un string de
 dos caracteres tiene una longitud de 2, y sus carácteres tendrán las posiciones
 0 y 1.
-Escribe una función contarFs que tome un string como su único argumento
-y devuelva un número que indica cuántos caracteres “F” en mayúsculas haya
-en el string.
+Escribe una función llamada contarCaracteres que se comporte
+como contarFs, excepto que toma un segundo argumento que indica el carácter
+que debe ser contado (en lugar de contar solo caracteres “F” en mayúscula).
+Reescribe contarFs para que haga uso de esta nueva función.
 
 */
-function contarFs(string) {
-  // Función string que recibe un string como parámetro
+function contarCaracteres(string, letra) {
+  // Función string que recibe como parámetro 2 string
   const largo = string.length; // declaramos y asignamos el largo del string
-  const letra = "F"; // Constante a evaluar
-  let contador = 0; // Contador de EFES
+  //const letra = "F"; // Constante a evaluar
+  let contador = 0; // Contador del buscador de caracteres
   for (let i = 0; i < largo; i++) {
     // Ciclo que recorre el string según su largo
     if (letra.includes(string[i])) {
@@ -23,6 +24,6 @@ function contarFs(string) {
       contador++;
     }
   }
-  console.log(`El String "${string}" contiene ${contador} F`); // Efecto secundario de contar F
+  console.log(`El String "${string}" contiene ${contador} "${letra}"s`); // Efecto secundario de contar F
 }
-contarFs("FELIPEFFFFFFFFFFFFFF"); // Llamado a la función
+contarCaracteres("FELIPEFFFFFFFFFFFFFF", "E"); // Llamado a la función
