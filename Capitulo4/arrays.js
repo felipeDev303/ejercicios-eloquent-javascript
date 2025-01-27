1a334b1536b1b303eb4470c5bc3dd0cb1f54c72a
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-26 21:27:20 -0300

    add arrays.js

---
+++ Capitulo4/arrays.js
@@ -0,0 +1,14 @@
+let diario = [
+  {
+    eventos: ["estudio", "trabajo", "comer", "entrenar", "cafe"],
+    developer: true,
+  },
+  {
+    eventos: ["estudio", "leer", "paseo", "entrenar", "mate"],
+    developer: true,
+  },
+  {
+    eventos: ["treking", "fiesta", "lago", "cerveza"],
+    developer: false,
+  },
+];
--- Capitulo4/listaDeNumeros.js
+++ Capitulo4/listaDeNumeros.js
@@ -4,3 +4,5 @@ let listaDeNumeros = [2, 3, 4, 6, 7, 9];
 console.log(listaDeNumeros[3]);
 console.log(listaDeNumeros[0]);
 console.log(listaDeNumeros[2 - 1]);
+
+console.log(typeof []); // -> object
