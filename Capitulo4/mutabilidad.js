273384072bf0a824dfd6a92f2df668169d16911a
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-01-28 01:28:47 -0300

    add no permitido

--- Capitulo4/mutabilidad.js
+++ Capitulo4/mutabilidad.js
@@ -8,3 +8,7 @@ console.log(objeto1 == objeto3); // false
 objeto1.valor = 15;
 console.log(objeto2.valor); // 15
 console.log(objeto3.valor); // 10
+
+const puntuacion = { visitantes: 0, locales: 0 };
+puntuacion.visitantes = 1; // permitido
+puntuacion = { visitantes: 1, locales: 1 }; // No permitido -> TypeError: Assignment to constant variable.
