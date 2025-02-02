5ae4f980c8ec79fe4d95ee758712862c83aac8b8
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-01 23:54:15 -0300

    script was modified

--- Capitulo4/array-slice.js
+++ Capitulo4/array-slice.js
@@ -1,13 +1,7 @@
 const str = "The quick brown fox jumps over the lazy dog.";
+console.log(str.length); // 44
 
-console.log(str.slice(31));
-// Expected output: "the lazy dog."
+// 1️. Si `indexEnd` es omitido: Extrae desde `indexStart` hasta el final de la cadena.
+console.log(str.slice(11)); // rown fox jumps over the lazy dog.
 
-console.log(str.slice(4, 19));
-// Expected output: "quick brown fox"
-
-console.log(str.slice(-4));
-// Expected output: "dog."
-
-console.log(str.slice(-9, -5));
-// Expected output: "lazy"
+// 2. Si indexStart >= str.length: Retorna una cadena vacía porque comienza en un índice fuera de rango.
