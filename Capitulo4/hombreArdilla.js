b144fc88cdd5793db481f180476bdbff0f71cdb8
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-04 23:57:09 -0300

    modified hombreArdilla

--- Capitulo4/hombreArdilla.js
+++ Capitulo4/hombreArdilla.js
@@ -10,3 +10,12 @@ function phi(tabla) {
   );
 }
 console.log(phi([76, 9, 4, 1])); // 0.06859943405700354
+
+// DESESTRUCTURAR: La función anterior es incomoda de leer, ya que hay una vinculación apuntando al array, otra forma es apuntar
+// a los elementos del array (let n00 = tabla[0] y así sucesivamente)
+function phi([n00, n01, n10, n11]) {
+  return (
+    (n11 + n00 - n10 * n01) /
+    Math.sqrt((n10 + n11) * (n00 + n01) * (n01 + n11) * (n00 + n10))
+  );
+}
