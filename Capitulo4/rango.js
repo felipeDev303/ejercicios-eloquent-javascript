4fb99e6d1f12bfd196c54a5bf0ab0f8892988276
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-07 23:55:00 -0300

    modified rango.js

--- Capitulo4/rango.js
+++ Capitulo4/rango.js
@@ -1,16 +1,31 @@
 // Función rango recibe 2 parámetros el inicio y el final del array
-function rango(inicio, final) {
+function rango(inicio, final, paso) {
   // array de todos los números
-  let listaDeNumeros = [inicio];
+  let listaDeNumeros = [];
   // Primer número del array
   let resultado = inicio;
 
   // Ciclo que itera rellenando el array desde el inicio al final
-  while (resultado < final) {
-    resultado++;
-    listaDeNumeros.push(resultado);
-    //listaDeNumeros.sort();
+  while (resultado !== final) {
+    if (paso === 0 || null) {
+      resultado++;
+      return listaDeNumeros.push(resultado);
+    } else if (paso < 0) {
+      resultado = resultado - paso;
+      return listaDeNumeros.push(resultado);
+    } else if (paso > 0) {
+      resultado = resultado + paso;
+      return listaDeNumeros.push(resultado);
+    } else resultado === final;
+
+    return listaDeNumeros;
   }
+
   console.log(listaDeNumeros);
+
+  const SUMANUM = listaDeNumeros.reduce((acc, el) => acc + el, 0);
+
+  console.log(SUMANUM);
 }
-rango(2, 50);
+
+rango(1, 10, 2);
