7de6cc5ce957510adf565f3ec5b74e74191fab83
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-11 02:32:59 -0300

    modified arrayList.js

--- Capitulo4/arrayLista.js
+++ Capitulo4/arrayLista.js
@@ -7,10 +7,23 @@ function Nodo(value) {
   this.next = null;
 }
 
+Lista.prototype.add = function (value) {
+  if (this.head === null) this.head = new Nodo(value);
+  else {
+    let referencia = this.head;
+    referencia.next = new Nodo(value);
+    while (referencia.next !== null) {
+      referencia = referencia.next;
+    }
+    referencia.next = new Nodo(valor);
+  }
+};
+
 let list = new Lista();
-let nodo = new Nodo(5);
+// let nodo = new Nodo(1); list.head = nodo;
+//let nodo2 = new Nodo(2); list.head.next = nodo2;
 
-list.head = nodo;
+list.add(1);
+list.add(2);
 
-console.log(nodo);
 console.log(list);
