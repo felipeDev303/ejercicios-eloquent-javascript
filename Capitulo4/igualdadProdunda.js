288843efe62f974a68bf1b44da9e853db2dd542f
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-15 22:45:57 -0300

    modified igualdadPrfunda.js

--- Capitulo4/igualdadProdunda.js
+++ Capitulo4/igualdadProdunda.js
@@ -1,1 +1,12 @@
-function igualdadProdunda() {}
+function igualdadProdunda(valor1, valor2) {
+  if (valor1 == valor2) {
+    return true;
+  } else {
+    console.log("Noooo");
+  }
+}
+
+let valor1 = 1; //{ nombre: "Felipe", apellido: "Morales" };
+let valor2 = 1; //{ nombre: "Felipe", apellido: "Morales" };
+
+console.log(igualdadProdunda(valor1, valor2));
