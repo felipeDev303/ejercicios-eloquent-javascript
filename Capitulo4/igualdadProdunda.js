adc0fee7007f56d06b57e3fc8f60ebc4250f8fe1
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-16 01:52:52 -0300

    modified igualdadProfunda

--- Capitulo4/igualdadProdunda.js
+++ Capitulo4/igualdadProdunda.js
@@ -1,12 +1,55 @@
-function igualdadProdunda(valor1, valor2) {
-  if (valor1 == valor2) {
-    return true;
+function igualdadProdunda(result1, result2) {
+  let type = typeof result1;
+  let type2 = typeof result2;
+
+  if (type && type2 == "number") {
+    if (result1 == result2) {
+      console.log("NÚMEROS IGUALES");
+    } else {
+      console.log("NÚMEROS DISTINTOS");
+    }
+  } else if (type && type2 === "object") {
+    const OBJECT1 = Object.entries(result1).map((entry) => {
+      const [key, value] = entry;
+      console.log({ key, value });
+    });
+
+    const OBJECT2 = Object.entries(result2).map((entry) => {
+      const value = result2[key];
+      console.log(key, value);
+    });
   } else {
-    console.log("Noooo");
+    console.log("Guarda los dientes ministro");
   }
 }
 
-let valor1 = 1; //{ nombre: "Felipe", apellido: "Morales" };
-let valor2 = 1; //{ nombre: "Felipe", apellido: "Morales" };
+//let result1 = 1; //{ news: [], profiles: { user: [] } };
+//let result2 = 2; // { news: [], profiles: { user: [] } };
 
-console.log(igualdadProdunda(valor1, valor2));
+igualdadProdunda(1, []);
+
+/* KEY
+const mappedResults = Object.keys(results1).map(key => {
+    console.log(key)
+})
+
+// VALUE
+const mappedResults = Object.keys(results1).map(key => {
+    const value = results1[key]
+    console.log(value)
+
+})
+
+ OBJECT VALUE
+const OBJECT = Object.values(results2);
+console.log(OBJECT);
+
+// OBJECT ENTRIES
+const OBJECT2 = Object.entries(results2);
+console.log(OBJECT2);
+
+// OBJECT ENTRIES
+const OBJECT3 = Object.entries(results2).map((entry) => {
+  const [key, value] = entry;
+  console.log({ key, value });
+});*/
