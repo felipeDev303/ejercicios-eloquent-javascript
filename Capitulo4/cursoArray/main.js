7d228d1a4803b9eba6c5084020d4b363b20b3ede
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-02-28 01:51:47 -0300

    modified cursoArray

--- Capitulo4/cursoArray/main.js
+++ Capitulo4/cursoArray/main.js
@@ -1,7 +1,27 @@
 let beers = ["erdinger", "corona", "delirium"];
+let wines = ["chardonnay", "merlot", "cabernet"];
+
+// Mutable
+beers.push("heineken"); // Agrega al final
+beers.unshift("modelo"); // Agrega al inicio
+beers.splice(2, 0, "bud"); // Insertar y reemplazar en un posición
+
+let b = beers.pop(); // Elimina el último y lo devuelve
+let b2 = beers.shift(); // Elimina el primero y lo devuelve
+console.log(b); // heineken
+console.log(b2); // modelo
+console.log(beers.indexOf("corona")); //2 (-1 si no existe, busqueda es estricta)
+
+// Inmutable
+//const alcoholicDrinks = beers.concat(wines);
+//alcoholicDrinks.push("carmenere");
+
+//const onlyWines = alcoholicDrinks.slice(4, 8);
+
+beers.fill("cervezas", 0, beers.length);
 
 showDom("array1", beers);
-showDom("array2", beers);
+showDom("array2", wines);
 
 function show(arr) {
   for (let i = 0; i < arr.length; i++) {
