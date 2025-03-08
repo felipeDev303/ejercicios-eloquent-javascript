d2064a61f4304387e7913d760bef2e7b4cdaee1e
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-07 23:30:41 -0300

    modified every

--- Capitulo4/every.js
+++ Capitulo4/every.js
@@ -1,9 +1,11 @@
 // Comprueba si Todos los elementos de la matriz pasan la prueba implementada por la función proporcionada.
 // Devuelve true, es como &&
 function todosSonPositivos(array) {
-  return array.every((x) => x > 0);
+  return array.every((x) => x >= 0);
 }
 
-const numeros = [1, 30, 50, 60, 29, 10, 13];
+const numeros = [1, -2, 50, 20, 29, 10, 13];
+const numeros2 = [1, 0, 50, 20, 29, 10, 13];
 
 console.log(todosSonPositivos(numeros));
+console.log(todosSonPositivos(numeros2));
