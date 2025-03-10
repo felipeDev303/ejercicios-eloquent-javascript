95b272a3f5f6e5ef78c647dc4fb13e738c872b4f
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-10 20:57:31 -0300

    add forEach, find & filter

--- Capitulo4/funcionesDeOrdenSuperior.js
+++ Capitulo4/funcionesDeOrdenSuperior.js
@@ -36,3 +36,44 @@ console.log(mayorQueSeis(5));
 const mayorQueVeinte = mayorQue(20);
 console.log(mayorQueVeinte(19));
 console.log(mayorQueSeis(21));
+
+/****************************************/
+/*********MÉTODOS DE BÚSQUEDA************/
+/**********Y TRANSFORMACIÓN**************/
+/****************************************/
+
+const numeros = [10, 45, 65, 32, 76, 99];
+const animales = ["Perro", "Gato", "Vaca", "Tortuga"];
+const productos = [
+  { nombre: "Cristal", categoria: "cerveza", precio: 800 },
+  { nombre: "Heineken", categoria: "cerveza", precio: 1000 },
+  { nombre: "Mistral", categoria: "pisco", precio: 7100 },
+  { nombre: "Alto del Carmen", categoria: "pisco", precio: 6900 },
+  { nombre: "Ramazzotti", categoria: "aperitivo", precio: 13000 },
+];
+/*
+// forEach() => Recorrer el array y ejecutar lo que digamos con cada elemento
+productos.forEach((producto) => {
+  console.log(producto.nombre);
+});
+
+// find() => Recorrer el array y retornar el primer elemento que cumpla con una condición
+
+const tortuga = animales.find((animal) => {
+  return animal === "Tortuga";
+});
+console.log(tortuga);
+
+let productoElegido = prompt("Ingrese el producto que quiere comprar");
+
+console.log(productos.find((producto) => producto.nombre === productoElegido));
+*/
+// filter() => Recorrer el array y retornar uno nuevo con todos los elementos que cumplan con esa condición
+
+const productosBaratos = productos.filter(
+  (producto) => producto.precio <= 1000
+);
+console.log(productosBaratos);
+
+const productosCaros = productos.filter((producto) => producto.precio > 1000);
+console.log(productosCaros);
