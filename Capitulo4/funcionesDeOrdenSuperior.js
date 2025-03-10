ec68158314b92870b1d825e366655d0a7b0d0eba
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-10 11:28:17 -0300

    modified funcionesDeOrdeSuperior.js

--- Capitulo4/funcionesDeOrdenSuperior.js
+++ Capitulo4/funcionesDeOrdenSuperior.js
@@ -1,8 +1,38 @@
 // ABSTRACCIÓN
-let total = 0;
-for (let i = 0; i < 10; i++) {
-  total += i;
-  console.log(total);
+function sumarRango(min, max) {
+  let total = 0;
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
+}
+console.log(sumarRango(1, 10));
+
+let sumaEntreUnoYDiez = sumarRango(1, 10);
+console.log(sumaEntreUnoYDiez);
+
+// RETORNO DE FUNCIONES
+
+// Función corta (una sola sentencia y un parámetro, el return es implicito y sin parentesis ni llaves)
+const mayorQueDiez = (num) => num > 10;
+console.log(mayorQueDiez(9));
+console.log(mayorQueDiez(11));
+
+// Esto es lo mismo
+const menorQueCuatro = (num) => {
+  return num < 4;
+};
+console.log(menorQueCuatro(3));
+console.log(menorQueCuatro(5));
+
+function mayorQue(x) {
+  return (num) => num > x;
 }
 
-//console.log(total);
+const mayorQueSeis = mayorQue(6);
+console.log(mayorQueSeis(7));
+console.log(mayorQueSeis(5));
+
+const mayorQueVeinte = mayorQue(20);
+console.log(mayorQueVeinte(19));
+console.log(mayorQueSeis(21));
