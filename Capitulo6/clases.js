4ccb8eb0587e5e5564e3a255989851c4fb42e35e
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-18 10:07:14 -0300

    add class

--- Capitulo6/clases.js
+++ Capitulo6/clases.js
@@ -16,3 +16,18 @@ let conejoRaro = new Conejo("raro");
 
 console.log(Object.getPrototypeOf(Conejo) == Function.prototype); // true
 console.log(Object.getPrototypeOf(conejoRaro) == Conejo.prototype); // true
+
+// Class (>2015)
+class Conejo {
+  constructor(tipo) {
+    this.tipo = tipo;
+  }
+  hablar(linea) {
+    console.log(`El conejo ${this.tipo} dice '${linea}'`);
+  }
+}
+let conejoAsesino = new Conejo("asesino");
+let conejoNegro = new Conejo("negro");
+
+console.log(conejoAsesino);
+console.log(conejoNegro);
