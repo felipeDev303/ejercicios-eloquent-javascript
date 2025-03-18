03d67c6706d709ac40694ac4a801f0fea2adcb57
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-18 01:14:56 -0300

    modified prototype.js

--- Capitulo6/prototype.js
+++ Capitulo6/prototype.js
@@ -1,6 +1,20 @@
 // Ejemplo
 let vacio = [];
 console.log(vacio.toString); // [Function: toString]
+console.log(vacio.toString());
 
 console.log(Object.getPrototypeOf({}) == Object.prototype); // true
 console.log(Object.getPrototypeOf(Object.prototype)); // null
+
+// Object.create
+
+let conejoPrototipo = {
+  hablar(linea) {
+    console.log(`El conejo ${this.tipo} dice '${linea}'`);
+  },
+};
+
+let conejoAsesino = Object.create(conejoPrototipo);
+conejoPrototipo.tipo = "asesino";
+conejoPrototipo.hablar("SKREEEE!");
+// El conejo asesino dice 'SKREEEE!'
