01792e9233aef5922e6e06a3883cf79a5061aed0
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-17 23:57:25 -0300

    modified prototype.js

--- Capitulo6/prototype.js
+++ Capitulo6/prototype.js
@@ -1,2 +1,6 @@
+// Ejemplo
+let vacio = [];
+console.log(vacio.toString); // [Function: toString]
+
 console.log(Object.getPrototypeOf({}) == Object.prototype); // true
 console.log(Object.getPrototypeOf(Object.prototype)); // null
