76e55b083d12a2b03eacfa8e8aa063d7f50be640
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-25 02:31:15 -0300

    modified polimorfismo & symbol

--- Capitulo6/polimorfismo.js
+++ Capitulo6/polimorfismo.js
@@ -1,3 +1,4 @@
+/*
 // Base class representing an Animal
 class Animal {
   makeSound() {
@@ -165,3 +166,52 @@ function multiply(a, b) {
 
 console.log(operate(3, 4, add)); // Outputs: 7
 console.log(operate(3, 4, multiply)); // Outputs: 12
+*/
+
+// CLASES: sin utilizar la palabra new
+/*function crearConejo(tipo) {
+  let conejo = Object.create(conejoPrototipo);
+  conejo.tipo = tipo;
+  return conejo;
+}
+
+
+// NEW
+function Conejo(tipo) {
+  this.tipo = tipo;
+}
+Conejo.prototype.hablar = function (linea) {
+  console.log(`El conejo ${this.tipo} dice '${linea}'`);
+};
+let conejoRaro = new Conejo("raro");
+
+//console.log(Object.getPrototypeOf(Conejo) == Function.prototype); // true
+//console.log(Object.getPrototypeOf(conejoRaro) == Conejo.prototype); // true
+*/
+// Class (>2015)
+class Conejo {
+  constructor(tipo) {
+    this.tipo = tipo;
+  }
+  hablar(linea) {
+    console.log(`El conejo ${this.tipo} dice '${linea}'`);
+  }
+}
+let conejoAsesino = new Conejo("asesino");
+let conejoNegro = new Conejo("negro");
+
+// Sobreescribir propiedades
+Conejo.prototype.dientes = "pequeños";
+//console.log(conejoAsesino.dientes); // pequeños
+
+conejoAsesino.dientes = "largos,filosos, y sangrientos";
+//console.log(conejoAsesino.dientes); // largos,filosos, y sangrientos
+
+//console.log(conejoNegro.dientes); //pequeños
+//console.log(Conejo.prototype.dientes); // pequeños
+
+Conejo.prototype.toString = function () {
+  return `un conejo ${this.tipo}`;
+};
+
+console.log(String(conejoNegro)); // un conejo negro
--- Capitulo6/symbol.js
+++ Capitulo6/symbol.js
@@ -1,3 +1,60 @@
+// Class (>2015)
+class Conejo {
+  constructor(tipo) {
+    this.tipo = tipo;
+  }
+  hablar(linea) {
+    console.log(`El conejo ${this.tipo} dice '${linea}'`);
+  }
+}
+let conejoAsesino = new Conejo("asesino");
+let conejoNegro = new Conejo("negro");
+
+// Sobreescribir propiedades
+Conejo.prototype.dientes = "pequeños";
+//console.log(conejoAsesino.dientes); // pequeños
+
+conejoAsesino.dientes = "largos,filosos, y sangrientos";
+//console.log(conejoAsesino.dientes); // largos,filosos, y sangrientos
+
+//console.log(conejoNegro.dientes); //pequeños
+//console.log(Conejo.prototype.dientes); // pequeños
+
+Conejo.prototype.toString = function () {
+  return `un conejo ${this.tipo}`;
+};
+
+console.log(String(conejoNegro)); // un conejo negro
+
 let simbolo = Symbol("nombre");
 console.log(simbolo == Symbol("nombre"));
 // -> false
+
+Conejo.prototype[simbolo] = 55;
+console.log(conejoNegro[simbolo]); // 55
+
+// Sobrecarga el comportamiento de los arrays agregando un nuevo método personalizado mediante símbolos.
+// Se crea un símbolo único (Los símbolos son únicos, incluso si tienen el mismo nombre interno)
+const simboloToString = Symbol("toString");
+
+//Se añade un método personalizado al prototipo de Array
+Array.prototype[simboloToString] = function () {
+  return `${this.length} cm de hilo azul`;
+};
+
+// Se comparan los resultados de toString() nativo y el nuevo método
+console.log([1, 2].toString());
+// -> 1,2
+console.log([1, 2][simboloToString]());
+// -> 2 cm de hilo azul
+
+//Creación del objeto con una propiedad de símbolo
+
+let objetoString = {
+  [simboloToString]() {
+    return "una cuerda de cañamo";
+  },
+};
+
+console.log(objetoString[simboloToString]());
+// → "una cuerda de cañamo"
