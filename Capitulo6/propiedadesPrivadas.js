0cda7ad46bed6b2f2217e3092eb4047b7aaa691b
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-28 11:36:08 -0300

    agregar clase CuentaBancaria con métodos para depositar, retirar y obtener saldo

--- Capitulo6/propiedadesPrivadas.js
+++ Capitulo6/propiedadesPrivadas.js
@@ -29,3 +29,40 @@ class RandomSource {
 let random = new RandomSource(1000000);
 
 console.log(random.getNumber());
+
+// Ejemplo Cuenta Bancaria
+class CuentaBancaria {
+  #saldo = 0;
+  #numeroDeCuenta = Math.floor(
+    Math.random(toString(36).substring(2, 9)) * 1000000
+  ); // Genera un número de cuenta aleatorio de 7 dígitos
+  constructor(saldoInicial) {
+    // Constructor que recibe el saldo inicial
+    this.#saldo = saldoInicial;
+  }
+
+  depositar(monto) {
+    this.#actualizarSaldo(monto);
+  }
+
+  retirar(monto) {
+    if (monto <= this.#saldo) {
+      this.#actualizarSaldo(-monto);
+    } else {
+      console.log("Fondos insuficientes");
+    }
+  }
+
+  obtenerSaldo() {
+    return `El saldo de la cuenta ${this.#numeroDeCuenta} es ${this.#saldo}`;
+  }
+
+  #actualizarSaldo(monto) {
+    this.#saldo += monto;
+  }
+}
+
+let JuanCuenta = new CuentaBancaria(100);
+JuanCuenta.depositar(50);
+JuanCuenta.retirar(30);
+console.log(JuanCuenta.obtenerSaldo()); // El saldo de la cuenta es 120
