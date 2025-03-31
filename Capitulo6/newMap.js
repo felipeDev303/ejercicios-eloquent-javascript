669e0d4fb441da8af46040ecf8b03ed654be01d7
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-03-31 03:06:07 -0300

    agregar ejemplos de uso de getters, setters y métodos estáticos en clases

--- Capitulo6/newMap.js
+++ Capitulo6/newMap.js
@@ -21,6 +21,8 @@ edades.set("Julia", 37);
 edades.set("Felipe", 36);
 edades.set("Liang", 38);
 
+console.log(edades.size); // -> 4
+
 // Obtener el valor de una clave
 console.log(`Julia tiene ${edades.get("Julia")}`);
 // -> Julia tiene 37
@@ -39,3 +41,99 @@ console.log({ x: 1 }.hasOwnProperty("x"));
 // Comprobar si existe una clave (en el prototipo)
 console.log({ x: 1 }.hasOwnProperty("toString"));
 // -> false
+
+// Los métodos getter se definen escribiendo get antes del nombre del método en una expresión de objeto o declaración de clase.
+// Se pueden usar para definir propiedades que se calculan dinámicamente en lugar de almacenarse directamente en el objeto.
+// Por ejemplo, el siguiente objeto tiene una propiedad size que devuelve un número aleatorio entre 0 y 100 cada vez que se accede a ella:
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100);
+  },
+};
+
+console.log(varyingSize.size); // -> 20
+console.log(varyingSize.size); // -> 71
+
+function speak(line) {
+  console.log(`El conejo ${this.type} dice '${line}'`);
+}
+
+let conejoBlanco = {
+  type: "blanco",
+  speak,
+};
+
+// Llamada al método desde el objeto
+conejoBlanco.speak("Hola");
+// -> El conejo blanco dice 'Hola'
+
+// Getter
+class Conejo {
+  constructor(type) {
+    this.type = type;
+  }
+  get speak() {
+    return `El conejo ${this.type} dice 'Hola'`;
+  }
+}
+
+let conejoNegro = new Conejo("Negro");
+console.log(conejoNegro.speak); // Acceso como propiedad
+// -> El conejo Negro dice 'Hola'
+
+// Getter vs Setter
+class Persona {
+  constructor(nombre) {
+    this.nombre = nombre;
+    this._edad = 0; // propiedad interna
+  }
+
+  get edad() {
+    return this._edad;
+  }
+
+  set edad(nuevaEdad) {
+    if (nuevaEdad < 0) {
+      console.log("La edad no puede ser negativa");
+    } else {
+      this._edad = nuevaEdad;
+    }
+  }
+}
+
+let persona = new Persona("Felipe");
+
+persona.edad = 36; // se llama al setter
+console.log(persona.edad);
+
+persona.edad = -5;
+console.log(persona.edad);
+
+// Métodos estáticos
+
+class Temperature {
+  constructor(celsius) {
+    this.celsius = celsius;
+  }
+
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
+  }
+
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+
+  static fromFahrenheit(value) {
+    return new Temperature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temperature(22);
+//console.log(temp.fahrenheit); // -> 71.6 ( se llama al getter)
+
+//temp.fahrenheit = 100; // se llama al setter
+//console.log(temp.celsius); // -> 37.77777777777778
+
+console.log(Temperature.fromFahrenheit(86)); // -> Temperature { celsius: 30 }
+// no es necesario instanciar la clase para usar el método estático
